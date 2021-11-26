f7d50e4944c652e01e31a0c2961c15c1b77d743d
beregovoydima <79249188+beregovoydima@users.noreply.github.com>
2021-11-26 17:35:21 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -128,12 +128,12 @@ function openCity(evt, cityName) {
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active1", "");
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active1";
+  evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
 
